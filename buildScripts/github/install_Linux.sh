d7465cd2324961fe2ccc0271d7f9ef472b133d31
iakov <iakov@users.noreply.github.com>
2024-10-07 12:57:19 +0300

    AltLinux CI: install proper version of Python

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -5,7 +5,7 @@ ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 
 if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
-    libudev-devel libGL-devel libGLX-mesa python3-dev zlib-devel make ccache python3-module-pip time \
+    libudev-devel libGL-devel libGLX-mesa python3.${TRIK_PYTHON3_VERSION_MINOR}-{dev,module-pip} zlib-devel make ccache time \
     rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
 else
     docker pull trikset/linux-builder
