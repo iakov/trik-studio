5819ad64d75e6fc2124fa3e84aa770b6df45a5c4
iakov <iakov@users.noreply.github.com>
2024-10-07 14:00:36 +0300

    Update install_Linux.sh

--- buildScripts/github/install_Linux.sh
+++ buildScripts/github/install_Linux.sh
@@ -5,7 +5,7 @@ ID=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 
 if [ "$ID" = "altlinux" ]; then
     apt-get update && apt-get install -y git-core gcc-c++ curl xz p7zip-standalone rsync libusb-devel \
-    libudev-devel libGL-devel libGLX-mesa python3.${TRIK_PYTHON3_VERSION_MINOR}-{dev,module-pip} zlib-devel make ccache time \
+    libudev-devel libGL-devel libGLX-mesa python3-{dev,module-pip}="3.${TRIK_PYTHON3_VERSION_MINOR}.*" zlib-devel make ccache time \
     rsync qt5-multimedia-devel qt5-svg-devel qt5-script-devel qt5-tools qt5-serialport-devel
 else
     docker pull trikset/linux-builder
